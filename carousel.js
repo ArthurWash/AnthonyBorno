981524b823662e51199f71a4b7fb94777d396c39
AJ <ajw53@NJIT.edu>
2024-02-08 15:45:40 -0500

    Update dropdown menus and add automatic gallery retrieval to gallery.js

--- carousel.js
+++ carousel.js
@@ -1,5 +1,6 @@
 /* Carousel Setup */
 
+// Select the carousel inner div element
 let carouselInnerDiv = document.querySelector('.carousel-inner');
 
 // Fetch the list of image filenames from the PHP script
@@ -11,18 +12,23 @@ fetch('https://anthonyborno.com/get-images.php') // Make sure to replace this wi
 
     console.log(images);
     Object.values(images).forEach((image, index) => {
+        // Create a div element for each carousel item
         let carouselItemDiv = document.createElement('div');
         carouselItemDiv.className = 'carousel-item';
         if(index === 0) {
             carouselItemDiv.classList.add('active');
         }
 
+        // Create an img element for each image
         let imgElement = document.createElement('img');
         imgElement.src = 'https://anthonyborno.com/carousel/' + image;
         imgElement.className = 'd-block mx-auto img-fluid';
         imgElement.alt = 'Image ' + (index + 1);
 
+        // Append the img element to the carousel item div
         carouselItemDiv.appendChild(imgElement);
+
+        // Append the carousel item div to the carousel inner div
         carouselInnerDiv.appendChild(carouselItemDiv);
     });
   });
--- gallery.js
+++ gallery.js
@@ -1,22 +1,31 @@
+// Configuring the lightbox options
 lightbox.option({
-    'resizeDuration': 0
-  });
-  
-  /* Gallery Setup */
-  $.getJSON('https://anthonyborno.com/get-gallery-images.php', function(images) {
-      var $gallery = $('#my-gallery');
+  'resizeDuration': 0
+});
+
+/* Gallery Setup */
+function loadGallery(galleryId) {
+  $.getJSON('https://anthonyborno.com/get-gallery-images.php?gallery=' + galleryId, function(images) {
+      var $gallery = $('#' + galleryId);
       $.each(images, function(i, image) {
-        var $link = $('<a>')
-          .attr('href', 'https://anthonyborno.com/' + image.original)
-          .attr('data-lightbox', 'my-gallery');
-        var $img = $('<img>').attr('src', 'https://anthonyborno.com/' + image.original);
-        $link.append($img);
-        $gallery.append($link);
+          var $link = $('<a>')
+              .attr('href', 'https://anthonyborno.com/' + image.original)
+              .attr('data-lightbox', galleryId);
+          var $img = $('<img>').attr('src', 'https://anthonyborno.com/' + image.original);
+          $link.append($img);
+          $gallery.append($link);
       });
-    
+
       $gallery.justifiedGallery({
-        rowHeight: 300,
-        margins: 20,
-        lastRow: 'nojustify',
+          rowHeight: 300,
+          margins: 20,
+          lastRow: 'nojustify',
       });
   });
+}
+
+// Load all galleries on the page
+$('.gallery').each(function() {
+    var galleryId = $(this).attr('id');
+    loadGallery(galleryId);
+});
