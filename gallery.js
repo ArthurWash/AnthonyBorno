2931b8bbab17de3b0bb9a66283d2250da1fd5ee9
AJ <ajw53@NJIT.edu>
2024-03-04 11:29:36 -0500

    Add comments explaining code in gallery.js

--- gallery.js
+++ gallery.js
@@ -1,31 +1,40 @@
 // Configuring the lightbox options
 lightbox.option({
-  'resizeDuration': 0
+    'resizeDuration': 0
 });
 
 /* Gallery Setup */
 function loadGallery(galleryId) {
-  $.getJSON('https://anthonyborno.com/get-gallery-images.php?gallery=' + galleryId, function(images) {
-      var $gallery = $('#' + galleryId);
-      $.each(images, function(i, image) {
-          var $link = $('<a>')
-              .attr('href', 'https://anthonyborno.com/' + image.original)
-              .attr('data-lightbox', galleryId);
-          var $img = $('<img>').attr('src', 'https://anthonyborno.com/' + image.original)
-          $link.append($img);
-          $gallery.append($link);
-      });
+    // Make a GET request to retrieve the gallery images from the server
+    $.getJSON('https://anthonyborno.com/get-gallery-images.php?gallery=' + galleryId, function(images) {
+            var $gallery = $('#' + galleryId);
+            // Iterate over each image in the response
+            $.each(images, function(i, image) {
+                    // Create a link element with the image URL as the href attribute
+                    var $link = $('<a>')
+                            .attr('href', 'https://anthonyborno.com/' + image.original)
+                            .attr('data-lightbox', galleryId);
+                    // Create an image element with the image URL as the src attribute
+                    var $img = $('<img>').attr('src', 'https://anthonyborno.com/' + image.original)
+                    // Append the image element to the link element
+                    $link.append($img);
+                    // Append the link element to the gallery container
+                    $gallery.append($link);
+            });
 
-      $gallery.justifiedGallery({
-          rowHeight: 300,
-          margins: 20,
-          lastRow: 'nojustify',
-      });
-  });
+            // Initialize the justified gallery plugin on the gallery container
+            $gallery.justifiedGallery({
+                    rowHeight: 300,
+                    margins: 20,
+                    lastRow: 'nojustify',
+            });
+    });
 }
 
 // Load all galleries on the page
 $('.gallery').each(function() {
-    var galleryId = $(this).attr('id');
-    loadGallery(galleryId);
+        // Get the ID of each gallery
+        var galleryId = $(this).attr('id');
+        // Call the loadGallery function for each gallery
+        loadGallery(galleryId);
 });
